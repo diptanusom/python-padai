200c7528a1993552e1097fa7d1658e27e73b494e
diptanusom <diptanu21191@iiitd.ac.in>
2021-08-25 12:50:37 +0530

    first commit

---
+++ filter.py
@@ -0,0 +1,14 @@
+"""def prime(x): 
+    if (x<2):
+        return False
+    else: 
+      for n in range (2,x):
+        if(x%n == 0 ):
+            return False
+    return True  
+print(list(filter(prime,range(100))))"""      
+import camelcase
+x=camelcase.CamelCase()
+a="sitir sudur keya morlo idur"  
+print(x.hump(a))   
+
